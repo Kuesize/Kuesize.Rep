c0c322cf27bcf671bf2743d4b193a73c41bf22a7
Kuesize <kuesize@gmail.com>
2024-01-01 14:41:51 +0300

    four commit

---
+++ JS/MainJS.js
@@ -0,0 +1,16 @@
+// eslint-disable-next-line quotes
+"use strict";
+
+const PrivJS = [];
+
+PrivJS[0] = prompt('Какой язык вам интересен больше всего?', '');
+PrivJS[1] = prompt('Как вас зовут?', '');
+
+console.log(PrivJS);
+
+const Otvet1 = PrivJS[0];
+const Otvet2 = PrivJS[1];
+
+alert(`Приветствую, ${Otvet2}! Надеюсь на моем сайты Ты сможешь найти ответы на вопросы, касающиеся языка ${Otvet1}`);
+
+
